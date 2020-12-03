1ab08c08ef255c9267104c5dce25cac6c3508e7b
BkJonathan <admin@sinphyukyun.com>
2020-12-03 10:50:03 +0630

    String Function

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+let firstName = "This is a string        ";
+console.log(firstName.length);
+console.log(firstName.charAt(2));
+console.log(firstName.indexOf("This"));
+console.log(firstName.replace("This","That"));
+console.log(firstName.slice(0,4));
+console.log(firstName.split(" "));
+console.log(firstName.toLowerCase());
+console.log(firstName.toUpperCase());
+console.log(firstName.trim());
 
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
