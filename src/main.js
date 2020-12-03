a7b56d3b033bd17ed3a72b3de98649df0de7e6bc
BkJonathan <admin@sinphyukyun.com>
2020-12-03 10:33:04 +0630

    Tmeplate String

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
-import Vue from 'vue'
-import App from './App.vue'
+function messageFunction(weather) {
+  let message = `It is ${weather} today`;
+  console.log(message);
+}
 
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+messageFunction("raining");
