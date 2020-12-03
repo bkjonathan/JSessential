db67b07a32803c8b163dd20a3cebf8aaac41faa9
BkJonathan <admin@sinphyukyun.com>
2020-12-03 09:38:52 +0630

    Function and statement Part

--- src/main.js
+++ src/main.js
@@ -1,8 +1,39 @@
-import Vue from 'vue'
-import App from './App.vue'
+//
+// function myFunc(name, weather, ...extraArgs) {
+//   console.log("Hello " + name + ".");
+//   console.log("It is " + weather + " today.");
+//   for (let i = 0; i < extraArgs.length; i++) {
+//     console.log("Extra Arg:" + extraArgs[i]);
+//   }
+// }
+// myFunc("Adam", "sunny", "one", "two", "three","four");
 
-Vue.config.productionTip = false
+// function myFunc(name) {
+//   return ("Hello " + name + ".");
+// }
+//
+// console.log(myFunc("Adam"));
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+// function myFunc(nameFunction) {
+//   return "Hello " + nameFunction() + ".";
+// }
+// console.log(
+//   myFunc(function() {
+//     return "Adam";
+//   })
+// );
+
+// function myFunc(nameFunction) {
+//   return "Hello" + nameFunction() + ".";
+// }
+// function printName(nameFunction, printFunction) {
+//   printFunction(myFunc(nameFunction));
+// }
+// printName(function() {
+//   return " Adam";
+// }, console.log);
+
+const myFunc = (nameFunction) => ("Hello" + nameFunction() + ".");
+const printName = (nameFunction, printFunction) => printFunction(myFunc(nameFunction));
+printName(function () { return "Adam" }, console.log);
+
