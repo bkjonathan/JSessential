f466c5add5d55498b51bdb807c6bf3ee2d7ba4a1
BkJonathan <kyawhtetsan.jar1990@gmail.com>
2020-12-29 18:20:52 +0630

    Data Type

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+// let mydata1 = 5 +5;
+// let mydata2 = 5+ ("5"*1);
+//
+// console.log('Result 1',mydata1)
+// console.log('Result 2',mydata2)
 
-Vue.config.productionTip = false
+let users = ['maung maung','aung aung','ma ma'];
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+// for (let i=0; i < users.length; i++){
+//     console.log('For Loop',users[i])
+// }
+//
+// users.forEach((user)=>{
+//     console.log('For Each Loop ',user)
+// })
+
+// console.log(users[0],users[1],users[2])
+console.log([...users,'ma khaing'])
