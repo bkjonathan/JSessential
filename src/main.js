9bbca311933d70275a4cc8b2630f862319669a76
BkJonathan <kyawhtetsan.jar1990@gmail.com>
2020-12-31 02:12:43 +0630

    Promise

--- src/main.js
+++ src/main.js
@@ -3,8 +3,15 @@ import * as math from './math'
 
 let data =[290,39,39,58,474,50];
 
-console.log('Addtional',math.addition(data))
-console.log('Multiply',math.multiply(data))
-console.log('Subtract',math.subtract(data))
-console.log('Divided',math.divied(10,2))
-console.log('Mean',math.mean(data))
+console.log('Addtional',math.addition(data));
+// math.asyncAdd(data).then((result)=>console.log(result))
+
+async function doTask() {
+    let total = await math.asyncAdd(data);
+    console.log(`Main Total: ${total}`);
+}
+doTask();
+// console.log('Multiply',math.multiply(data))
+// console.log('Subtract',math.subtract(data))
+// console.log('Divided',math.divied(10,2))
+// console.log('Mean',math.mean(data))
--- src/math/index.js
+++ src/math/index.js
@@ -1,8 +1,19 @@
-import addition from './sum';
+import addition from "./sum";
 
 export function mean(values) {
-    return addition(values)/values.length;
+  return addition(values) / values.length;
 }
 
-export {addition}
-export * from './operation';
+export function asyncAdd(values) {
+  return new Promise((resolve)=>{
+    setTimeout(() => {
+      let total = addition(values);
+      console.log(`Async Total: ${total}`);
+      resolve(total);
+    }, 2000);
+  });
+
+}
+
+export { addition };
+export * from "./operation";
