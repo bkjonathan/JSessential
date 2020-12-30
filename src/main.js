3213e171c97d0fbbcda07759f3f7f08c7ca6c96f
BkJonathan <kyawhtetsan.jar1990@gmail.com>
2020-12-30 13:26:26 +0630

    Array Function

--- src/main.js
+++ src/main.js
@@ -1,8 +1,38 @@
-import Vue from 'vue'
-import App from './App.vue'
+// let users = ["Thomas","Soe gyi","Kyaw Gyi","Aung Aung"];
+let users = [
+  {
+    name: "Thomas",
+    age:30,
+    income:1000000
+  },
+  {
+    name: "Soe gyi",
+    age:40,
+    income:1200000
+  },
+  {
+    name: "Kyaw Gyi",
+    age:20,
+    income:1300000
+  },
+  {
+    name: "Aung Aung",
+    age:23,
+    income:1400000
+  }
+];
+// let newUser = users.map((user)=> ({
+//   user
+// }));
 
-Vue.config.productionTip = false
+// console.log(newUser);
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+
+// let filterArray = users.find((user)=>user.age <= 25);
+// users.reverse()
+// console.log(filterArray);
+// let totalIncome = users.reduce((total,user)=>(total += user.income),0);
+let findIn = users.findIndex((user)=>user.name == "Aung Aung")
+users.splice(findIn,1)
+console.log(users);
+
